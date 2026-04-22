a424168bed514be811802d05e56eedc470ff2782
moiz045 <moiz23642@gmail.com>
2026-04-22 19:30:42 +0500

    add file upload middlewares

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-app.use(cors());
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(cookieParser());
 app.listen(PORT, () => {
