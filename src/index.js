df4c4126ce17e47bb7bb48a571b0589716061f38
moiz045 <moiz23642@gmail.com>
2026-04-22 20:34:14 +0500

    routes added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { connectDB } from "./config/db.config.js";
+import router from "./routes/user.routes.js";
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ connectDB();
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(cookieParser());
+
+app.use("/api/v1/users", router);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
